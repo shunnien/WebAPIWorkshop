6547bc56495c75b3dc0e1b9917e0e8ef65d97a37
AllenYu <wiamyu@gmail.com>
2016-09-28 21:15:50 +0800

    複雜型別指定[FromUri]

---
+++ FromUriBody/Controllers/ModelBindingController.cs
@@ -0,0 +1,43 @@
+﻿using FromUriBody.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FromUriBody.Controllers
+{
+    public class ModelBindingController : ApiController
+    {
+        /// <summary>
+        /// 複雜型別指定[FromUri]
+        /// </summary>
+        /// <param name="location">The location.</param>
+        /// <returns>IHttpActionResult.</returns>
+        public IHttpActionResult Get([FromUri] GeoPoint location)
+        {
+            if (location.Latitude == 0 || location.Longitude == 0)
+            {
+                return BadRequest("Location is empty!");
+            }
+            var geo = $"Latitude:{location.Latitude},Longitude:{location.Longitude}";
+            return Ok(geo);
+        }
+
+        // POST: api/ModelBinding
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ModelBinding/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ModelBinding/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FromUriBody/Models/GeoPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FromUriBody.Models
+{
+    public class GeoPoint
+    {
+        // 緯度
+        public double Latitude { get; set; }
+        // 經度
+        public double Longitude { get; set; }
+
+    }
+}
