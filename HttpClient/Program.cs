c27f4e017a61619956ece024e1fd9e0281ac3efb
AllenYu <wiamyu@gmail.com>
2016-09-28 19:02:11 +0800

    針對 Web API 重新封裝的 API，API 重新使用大量的非同步語法重新設計，以更加符合現代化 Web API 存取的需求。
    請至 http://requestb.in 申請一個測試 URI。
    GetStringAsync

--- HttpClient/Program.cs
+++ HttpClient/Program.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace HttpClient
 {
     class Program
     {
+        private static string requestbinUrl = "http://requestb.in/1k80fbq1";
+
         static void Main(string[] args)
         {
+        }
+
+        
+
+        public async static Task ReUseHttpClient(System.Net.Http.HttpClient client)
+        {
+            // instance re-use
+            var task1 = await client.GetStringAsync(requestbinUrl);
+            var task2 = await client.GetStringAsync(requestbinUrl);
+
+            Console.WriteLine(task1);
+            Console.WriteLine(task2);
         }
     }
 }
