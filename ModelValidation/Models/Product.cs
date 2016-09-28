70d514d1316be0ffe107b6e0bb43493a14bb7454
AllenYu <wiamyu@gmail.com>
2016-09-28 21:36:50 +0800

    修改 Price 屬性

--- ModelValidation/Models/Product.cs
+++ ModelValidation/Models/Product.cs
@@ -11,6 +11,7 @@ namespace ModelValidation.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Required]
         public decimal Price { get; set; }
         [Range(0, 999)]
         public double Weight { get; set; }
