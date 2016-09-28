20d78167d40c151dd9e8cf6d1c7b14c45ee1648a
AllenYu <wiamyu@gmail.com>
2016-09-28 18:54:20 +0800

    轉換專案為 Console 專案

--- HttpClient/Properties/AssemblyInfo.cs
+++ HttpClient/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("85e71af9-fc7a-409f-99b4-f160f44d224d")]
+[assembly: Guid("9ace06a5-436a-4209-8e7b-06ca72a01bde")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
