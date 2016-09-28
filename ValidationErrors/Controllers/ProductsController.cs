ebe48183c14d14d3612f58ab5d9f8eb7bfb42454
AllenYu <wiamyu@gmail.com>
2016-09-28 21:55:34 +0800

    如果不想全域處理，也可以在 API Controller 或 API Action 層級去設置 Filter 屬性

---
+++ ValidationErrors/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using ModelValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ValidationErrors.Utility.Filter;
+
+namespace ValidationErrors.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Products/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Posts the specified product.
+        /// </summary>
+        /// <param name="product">The product.</param>
+        /// <returns>HttpResponseMessage.</returns>
+        [ModelValidationFilter]
+        public HttpResponseMessage Post(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                // Todo: 用 product 做些事
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ValidationErrors/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelValidation.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Range(0, 999)]
+        public double Weight { get; set; }
+    }
+}
