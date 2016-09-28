c0dc372eae157e6258335a3c2cb35945f50c9199
AllenYu <wiamyu@gmail.com>
2016-09-28 21:53:05 +0800

    建立一個 Filter 來統一處理 Model Validation 在 API Action 被呼叫前的狀態處理

---
+++ ValidationErrors/Utility/Filter/ModelValidationFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace ValidationErrors.Utility.Filter
+{
+    public class ModelValidationFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            // 在回應本體回傳驗證錯誤訊息
+            if (actionContext.ModelState.IsValid == false)
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(
+                    HttpStatusCode.BadRequest, actionContext.ModelState);
+            }
+        }
+    }
+}
