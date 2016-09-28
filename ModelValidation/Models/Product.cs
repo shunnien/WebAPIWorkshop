56edfa88c6f4da4cf536ac72de774c13b2b77b3d
AllenYu <wiamyu@gmail.com>
2016-09-28 21:23:45 +0800

    建立 `Product` 類別

---
+++ ModelValidation/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelValidation.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        [Range(0, 999)]
+        public double Weight { get; set; }
+    }
+}
